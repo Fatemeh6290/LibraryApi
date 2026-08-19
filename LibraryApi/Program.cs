2244e20327a2a29538e43d263e2f3032af392282
Fatemeh Naseri <f.naseri@cp-pro.de>
2026-08-19 15:29:17 +0200

    Swagger für das Projekt hinzugefügt

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -4,12 +4,21 @@ using LibraryApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IBookService, BookService>();
 builder.Services.AddSingleton<IMemberService, MemberService>();
 builder.Services.AddSingleton<ILoanService, LoanService>();
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
